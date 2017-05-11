2d3f12e949b5768289be789c64acb838082ca7d0
Scott Schmidt <sjschmidt44@gmail.com>
2017-05-11 09:20:47 -0700

    add app.json for CI testing

--- routes/gallery-routes.js
+++ routes/gallery-routes.js
@@ -18,6 +18,7 @@ module.exports = function(router) {
 
     galleryCtrl.fetchGallery(req.params.id)
     .then(gallery => {
+      debugger; // NOTE Debugger active
       if(gallery.userId.toString() !== req.user._id.toString()) {
         return res.status(401).send('Invalid user')
       }
